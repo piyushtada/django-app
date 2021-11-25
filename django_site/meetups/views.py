2787d63cab7f8ec2bec564cb2fb217bcb165e1fc
Piyush <piyushtada@gmail.com>
2021-11-25 21:12:55 +0100

    before details page added

--- django_site/meetups/views.py
+++ django_site/meetups/views.py
@@ -5,8 +5,16 @@ from django.shortcuts import render
 
 def index(request):
     meetups = [
-        {'title': 'A First Meetup'},
-        {'title': 'A Second Meetup'}
+        {
+            'title': 'A First Meetup',
+            'locations': 'New York, NY',
+            'slug': 'a-first-meetup'
+        },
+        {
+            'title': 'A Second Meetup',
+            'locations': 'Paris',
+            'slug': 'a-second-meetup'
+        }
     ]
     return render(request, 'meetups/index.html', {
         'show_meetups': False,
